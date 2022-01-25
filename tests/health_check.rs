a6b19756c5b4d4c5ecd2b9f61e27e81e2b4f9ddd
Baptiste <baptiste.lemorlec@imascap.com>
2022-01-25 13:39:14 +0100

    Refacto integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,27 +1,27 @@
-// `tokio::test` is the testing equivalent of `tokio::main`.
-// It also spares you from having to specify the `#[test]` attribute. //
-// You can inspect what code gets generated using
-// `cargo expand --test health_check` (<- name of the test file)
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listerner = TcpListener::bind("127.0.0.1:0").unwrap();
+    let port = listerner.local_addr().unwrap().port();
+    let server = zero2prod::run(listerner).expect("Failed to spawn server app");
+
+    let _ = actix_web::rt::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[actix_web::test]
 async fn health_check_works() {
-    spawn_app();
+    let addr = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", addr))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-// Launch our application in the background ~somehow~
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to spawn server app");
-
-    let _ = actix_web::rt::spawn(server);
 }
