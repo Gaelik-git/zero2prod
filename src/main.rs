8b8ac01c06e220d08189071a8acaa4c2b1673af4
Baptiste <baptiste.lemorlec@imascap.com>
2022-01-29 17:19:24 +0100

    Better conf of DB

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use zero2prod::{
     configuration::get_configuration,
@@ -15,9 +14,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Faild to connect to DB");
+    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
